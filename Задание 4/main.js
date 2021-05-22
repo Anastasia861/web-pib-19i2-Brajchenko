96c82168272754c755a6e27156467f59d54c820a
Anastasia861 <78462136+Anastasia861@users.noreply.github.com>
2021-05-22 12:49:29 +0600

    Add files via upload

---
+++ Задание 4/main.js
@@ -0,0 +1,11 @@
+function BuyFunction() {
+	alert("Сейчас это сделать невозможно!");
+    }
+	
+function LoadFunction() {
+    document.getElementById("demo").innerHTML = "Идет скачивание ... ";
+    }
+
+function SearchFunction() {
+	alert("Идет поиск ...");
+    }
